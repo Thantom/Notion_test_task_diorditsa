896ed730d584b8336f0d4128092e2c4a9bf0c090
Thantom <nervashe@mail.ru>
2026-05-21 20:37:25 +0300

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  use: {
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
+  },
+
+  reporter: [
+    ['html']
+  ]
+});
